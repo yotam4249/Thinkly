6bb33502440f795cfec59f52eda79872cdae9ac0
yotam4249 <136727458+yotam4249@users.noreply.github.com>
2025-10-28 14:56:30 +0200

    ui fixes 2

--- frontend/client/src/components/home/ChatList.tsx
+++ frontend/client/src/components/home/ChatList.tsx
@@ -1,20 +1,3 @@
-// import type { ChatListItem } from "../../types/chatList.type";
-// import { ChatItem } from "./ChatItem";
-
-// type Props = {
-//   items: ChatListItem[];
-//   onOpen: (id: string) => void;
-// };
-
-// export function ChatList({ items, onOpen }: Props) {
-//   return (
-//     <ul className="chat-list">
-//       {items.map((c) => (
-//         <ChatItem key={c.id} item={c} onOpen={onOpen} />
-//       ))}
-//     </ul>
-//   );
-// }
 import type { ChatListItem } from "../../types/chatList.type";
 import { ChatItem } from "./ChatItem";
 import { ChatActions } from "./ChatActions";
